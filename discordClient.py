0c05c7e51ee5b4c48ec8eaa8a7df41fcc43d00dc
E Smith <ers@tisp.us>
2023-04-20 00:13:34 -0400

    bot able to type in channel via func

--- discordClient.py
+++ discordClient.py
@@ -1,9 +1,10 @@
+from ast import arguments
 import interactions
-from interactions import Client, Intents, listen, slash_command
+from interactions import Client, Intents, listen, slash_command, slash_option
 import os
 
-vc : interactions.TYPE_VOICE_CHANNEL | interactions.GuildChannel= ''
-
+vc : interactions.TYPE_VOICE_CHANNEL | interactions.GuildChannel=''
+tc : interactions.TYPE_MESSAGEABLE_CHANNEL | interactions.GuildChannel=''
 # intents are what events we want to receive from discord, `DEFAULT` is usually fine
 bot = Client(intents=Intents.DEFAULT | Intents.MESSAGE_CONTENT, token=os.environ['DISCORD_TOKEN'])
 
@@ -20,24 +21,62 @@ async def on_message_create(event):
 
 @slash_command(name="test", description="classic.")
 async def my_command_function(ctx: interactions.InteractionContext):
-    await ctx.send("Hello World!")
+    global tc
+    await ctx.send("Hello World! :]")
+    tc = ctx.channel
 
 @slash_command(name="dictate_twitch", description="Pass twitch chat to SeaVoice tts")
 async def dictate_twitch_function(ctx: interactions.InteractionContext):
-    global vc
-    if (ctx.author.voice): # If the person is in a channel
+    global vc, tc
+
+    if (ctx.author.voice and not ctx.voice_state):
+        # if we haven't already joined a voice channel
+        # join the authors vc
+        await ctx.send("Joining...")
+        await ctx.author.voice.channel.connect()
         vc = ctx.author.voice.channel
-        await ctx.bot.connect_to_vc(vc._guild_id, ctx.author.voice._channel_id)
+        tc = ctx.channel
         await ctx.send('Bot joined')
+
+        #await ctx.bot.connect_to_vc(vc._guild_id, ctx.author.voice._channel_id)
     else: #But is (s)he isn't in a voice channel
         await ctx.send("You must be in a voice channel first so I can join it.")
 
 @slash_command(name="stop")
 async def stop_function(ctx: interactions.InteractionContext):
-    if (ctx.voice_client): # If the bot is in a voice channel 
-        await ctx.guild.voice_client.disconnect() # Leave the channel
+    global vc, tc
+    print(ctx.bot.get_bot_voice_state(ctx.guild_id))
+    print(vc)
+    print(ctx.bot.get_bot_voice_state(ctx.guild.id))
+    await ctx.send("ran /stop")
+    return
+    await ctx.voice_state.channel.disconnect()
+    return
+    await vc.disconnect()
+    if (bot.voice_client): # If the bot is in a voice channel 
+        await bot.voice_client.disconnect() # Leave the channel
         await ctx.send('Bot left')
     else: # But if it isn't
         await ctx.send("I'm not in a voice channel, use the join command to make me join")
 
+
+@slash_command(name="repeat")
+@slash_option(
+    name="msg",
+    description="message",
+    required=True,
+    opt_type=interactions.OptionType.STRING
+)
+async def repeat_function(ctx: interactions.InteractionContext, msg: str):
+    global tc
+    tc = ctx.channel
+    await repeat_text(msg)
+    await ctx.send(":)")
+
+async def repeat_text(msg : str):
+    global tc
+    await tc.send(f"{msg}", tts=True)
+
+
+bot.load_extension("interactions.ext.jurigged")
 bot.start()
